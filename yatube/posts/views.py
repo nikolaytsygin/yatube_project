79056779092e651e7b09c11e1870e55c94ea4cd1
Николай Цыгин <nikolay.tsygin@yandex.ru>
2021-12-23 22:36:44 +0300

    template ok

--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -1,7 +1,9 @@
 from django.http import HttpResponse
+from django.shortcuts import render
 
 def index(request):
-    return HttpResponse('Главная страница')
+    template = 'posts/index.html'
+    return render(request, template)
     # Create your views here.    
 def group_posts(request, pk):
     return HttpResponse('Групповой пост<slug>')
