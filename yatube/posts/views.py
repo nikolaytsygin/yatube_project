8c39dd51a6ca3f53115060199795b6e7c792ae41
nikolaytsygin <90354173+nikolaytsygin@users.noreply.github.com>
2022-01-01 20:12:21 +0300

    +{{title}}

--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -1,10 +1,21 @@
-from django.http import HttpResponse
+"""from django.http import HttpResponse."""
 from django.shortcuts import render
+
 
 def index(request):
     template = 'posts/index.html'
-    return render(request, template)
+    context = {
+        'text': 'Это главная страница проекта Yatube',
+        'title': 'Последние обновления на сайте'
+    }
+    return render(request, template, context)
     # Create your views here.
+
+
 def group_posts(request):
     template = 'posts/group_list.html'
-    return render(request, template)
+    context = {
+        'text': 'Здесь будет информация о группах проекта Yatube',
+        'title': 'Лев Толстой – зеркало русской революции.'
+    }
+    return render(request, template, context)
