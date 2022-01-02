2f4b5c2c89cc8119068a4e5bc5419c96c0bbb689
nikolaytsygin <90354173+nikolaytsygin@users.noreply.github.com>
2022-01-02 18:12:13 +0300

    correct

--- yatube/posts/urls.py
+++ yatube/posts/urls.py
@@ -3,6 +3,6 @@ from . import views
 
 app_name = 'posts'
 urlpatterns = [
-    path('', views.index, name = 'posts_main'),
-    path('group_posts/', views.group_posts),
+    path('', views.index, name='posts_main'),
+    path('group_posts/', views.group_posts, name='post_group'),
 ]
